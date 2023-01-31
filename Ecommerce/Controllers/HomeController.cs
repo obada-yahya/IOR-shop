f452cca773d546baee136f221d02c268b7ae854b
Obada-Yahya <obadayahya.an@gmail.com>
2023-01-31 21:38:05 +0200

    About us Page

--- Ecommerce/Controllers/HomeController.cs
+++ Ecommerce/Controllers/HomeController.cs
@@ -32,6 +32,7 @@ namespace Ecommerce.Controllers
 		{
 			return View();
 		}
+		public IActionResult AboutUs() { return View(); }
 		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
 		public IActionResult Error()
 		{
